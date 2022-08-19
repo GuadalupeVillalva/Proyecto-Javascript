66d8c88fb73f7242f904a7a4a3db0ed2a1a88e96
Guadalupe Villalva <guadi_lupev@hotmail.com>
2022-08-19 02:09:52 -0300

    desafio-simulador-interactivo

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
+// mediante propmt solicito la información
+const tipoDeMoneda = prompt("¿Qué moneda desea vender/comprar?");
+const tipoDeCambio = prompt("¿Desea vender o comprar?");
+const cantidad = prompt("Si desea comprar ingrese el monto pesos argentinos, si desea vender ingrese el monto en la divisa");
 
-for(let i = 1; i < 11; i++){ // bucle que itera 10 veces y asigna turnos
-    let nombre = prompt("Ingrese su nombre"); // pido nombre
-    let apellido = prompt("Ingrese su apellido"); // pido apellido
-    if(nombre != "" && apellido != ""){ // itera mientras que lo ingresado no sea un espacio en blanco
-        alert(nombre + " " + apellido + " se le ha asignado el turno N° " + i); // devuelvo alert con turno
-    } else {
-        alert("No es válido, por favor ingrese su nombre y apellido") // le avisa al usuario que es inválido
-    }
+// declaro una función para vender
+function vender(valorDivisa){
+    let resultado = parseInt(cantidad) * valorDivisa;
+    alert("Es equivalente a " + resultado + " pesos argentinos." );
+};
+
+
+//declaro una función para comprar
+function comprar(divisa, valorDivisa){
+    let resultado = parseInt(cantidad) / valorDivisa;
+    alert("Es equivalente a " + resultado + " " + divisa);
+};
+
+
+// si se ingresa un número o texto donde no se corresponde o no se ingresa nada se alerta que no es válido
+if((tipoDeMoneda === !NaN || tipoDeMoneda === "") || (tipoDeCambio === !NaN || tipoDeCambio === "") || (cantidad === NaN || cantidad === "")) {
+    alert("No es válido")
+} else if(tipoDeCambio === "VENDER" && tipoDeMoneda === "EURO"){ // Vender euro
+    vender(138);
+} else if(tipoDeCambio === "COMPRAR" && tipoDeMoneda === "EURO"){ // Comprar euro
+    comprar("euros", 140);
+} else if(tipoDeCambio === "VENDER" && tipoDeMoneda === "DOLAR"){ // Vender dólar
+    vender(292);
+} else if(tipoDeCambio === "COMPRAR" && tipoDeMoneda === "DOLAR"){ // Comprar Dólar
+    comprar("dólares", 297);
 }
